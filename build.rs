0c2a828bf328ae9927eb203dcb866db3ead41694
Kim Goetzke <kbgoetzke@gmail.com>
2025-05-03 06:52:28 +0100

    Add information to manifest

--- build.rs
+++ build.rs
@@ -3,20 +3,37 @@ extern crate winres;
 
 #[cfg(windows)]
 fn main() {
+  let version = env!("CARGO_PKG_VERSION");
   let mut res = winres::WindowsResource::new();
-  res.set_manifest(
+  res.set("FileDescription", "A windows management utility for Windows");
+  res.set("ProductName", "Randolf");
+  res.set("InternalName", "Randolf");
+  res.set("OriginalFilename", "randolf.exe");
+  res.set("LegalCopyright", "© 2025 Kim Goetzke");
+  res.set_manifest(&format!(
     r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+  <assemblyIdentity
+      version="{version}.0"
+      processorArchitecture="*"
+      name="kimgoetzke.Randolf"
+      type="win32" />
+  <description>A window management utility for Windows</description>
+  <compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1">
+    <application>
+      <supportedOS Id="{{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}}"/>
+    </application>
+  </compatibility>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
     <security>
-        <requestedPrivileges>
-            <requestedExecutionLevel level="asInvoker" uiAccess="false" />
-        </requestedPrivileges>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="asInvoker" uiAccess="false" />
+      </requestedPrivileges>
     </security>
-</trustInfo>
+  </trustInfo>
 </assembly>
 "#,
-  );
+  ));
   res.compile().unwrap();
 }
 
